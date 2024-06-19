ba0fad354bf53ac5dcf67b04a4e8c76a41ee7705
Vineet Tapkire <vineet.mynt@perennialsys.com>
2024-06-19 14:49:28 +0530

    PereLiDAR-scanner #commit basic code structure pushed

---
+++ PereLiDAR-scanner/PereLiDAR-scanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PereLiDAR-scanner
+//
+//  Created by Perennial on 19/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ PereLiDAR-scanner/PereLiDAR-scannerUITests/PereLiDAR_scannerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  PereLiDAR_scannerUITestsLaunchTests.swift
+//  PereLiDAR-scannerUITests
+//
+//  Created by Perennial on 19/06/24.
+//
+
+import XCTest
+
+final class PereLiDAR_scannerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
